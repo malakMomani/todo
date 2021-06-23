eb4a8d89f6ccde5e6034f92de190561e47c6c163
Malak Al-Momani <malak.momani.96@gmail.com>
2021-06-24 01:46:34 +0300

    add basics

--- src/app.js
+++ src/app.js
@@ -8,12 +8,30 @@ import PaginationProvider from './context/paginationContext';
 export default function App() {
   return (
     <>
+
+            <Login/>
+            <Auth action="delete">
+                <button>Fake Delete</button>
+            </Auth>
+            <Auth action="update">
+                <button>Fake Update</button>
+            </Auth>
+            <Auth action="create">
+                <button>+ Create a new item</button>
+            </Auth>
+            <Auth action="read">
+                <div>Fake List ..</div>
+            </Auth>
+<AuthProvider>
+
       <SettingsProvider>
         <PaginationProvider>
           <Header />
           <ToDo />
         </PaginationProvider>
       </SettingsProvider>
+</AuthProvider>
+
     </>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SignIn from '../auth/signin.js';
 
 
 function Header() {
@@ -9,6 +10,7 @@ function Header() {
       <Navbar bg="primary" variant="dark">
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
+          <SignIn />
         </Nav>
       </Navbar>
     </>
